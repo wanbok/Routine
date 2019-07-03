f4e3455556e4e807780c906dd21a508628fba93d
Choi, Wanbok <wanbok.choi@linecorp.com>
2019-07-03 22:30:11 +0900

    Add the task list as a boilerplate

---
+++ lib/task/task_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskList extends StatefulWidget {
+  TaskList({Key key}) : super(key: key);
+
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(); // TODO: Add list
+  }
+}
