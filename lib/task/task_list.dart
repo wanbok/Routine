0c38668458b02e3924a2e7fed651a6328633cddf
Choi, Wanbok <wanbok.choi@linecorp.com>
2019-07-04 22:34:13 +0900

    Add TaskItem's boilerplate

--- lib/task/task_list.dart
+++ lib/task/task_list.dart
@@ -10,6 +10,24 @@ class TaskList extends StatefulWidget {
 class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
-    return ListView(); // TODO: Add list
+    return ListView.builder( // TODO: Apply data
+      itemCount: 1,
+      itemBuilder: (context, index) => TaskItem(),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/sample.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: null /* add child content here */,
+      );
   }
 }
